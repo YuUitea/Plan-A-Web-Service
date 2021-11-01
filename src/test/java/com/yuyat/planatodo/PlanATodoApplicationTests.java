c00a37057d8b385c69bba201b78c1f4490b0cd1c
yuyat <dora.da.real@gmail.com>
2021-10-31 17:01:04 -0700

    Initiate Spring Boot web service for PlanA Todo App

---
+++ src/test/java/com/yuyat/planatodo/PlanATodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuyat.planatodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanATodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
